fc86d1db59b2df4443674f8865a2e177c14fa391
FRANCISCO GONGORA <fgongora67@gmail.com>
2015-02-08 17:30:59 -0500

    new class for feature 1.2

---
+++ VSRepositoryDemo/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSRepositoryDemo
+{
+    class Class2
+    {
+        //working on feature 1.2
+
+    }
+}
